ceff9b26cd4f888ec6d70ce684f9e14597d36b3a
Eike Siewertsen <eike@technocreatives.com>
2020-05-19 12:23:45 +0200

    add desktop keyboard bulk create scripts

--- actions/new_keyboards.sh
+++ actions/new_keyboards.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REGEX=$1
@@ -6,12 +7,11 @@ REGEX=$1
 # Get all the repos and add CI
 gut clone -o giellalt -r $REGEX
 gut checkout -o giellalt -r $REGEX -b develop
-gut ci export -t ~/git/divvun-ci-config/actions/speller -o giellalt -r $REGEX --output /tmp/x.toml --script $SCRIPTPATH/autoconf_json.sh
-gut ci generate -t ~/git/divvun-ci-config/actions/speller -o giellalt -r $REGEX -d /tmp/x.toml
+gut ci export -t $SCRIPTPATH/keyboard -o giellalt -r $REGEX --output /tmp/x.toml --script $SCRIPTPATH/autoconf_json.sh
+gut ci generate -t $SCRIPTPATH/keyboard -o giellalt -r $REGEX -d /tmp/x.toml
 rm /tmp/x.toml
 gut commit -o giellalt -r $REGEX -m "Add initial CI configuration"
 
 # Get all the repos and add them to the pahkat repo properly
 export PAHKAT_REPO=$2
-gut apply -o giellalt -r $REGEX -s "$SCRIPTPATH/init_pahkat_pkg_speller.sh"
-
+gut apply -o giellalt -r $REGEX -s "$SCRIPTPATH/init_pahkat_pkg_keyboard.sh"
